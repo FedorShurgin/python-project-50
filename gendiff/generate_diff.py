1af2dd577d100fafa0acd9297bfa6c0ff6bd841b
Fedor.Shurgin <shurginfedor@gmail.com>
2024-05-15 22:39:34 +0300

    fix

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,11 +1,11 @@
 from gendiff.building_diff import building_diff
-from gendiff.stylish import format_stylish
-from gendiff.plain import format_plain
-from gendiff.json import format_json
+from gendiff.formatters.stylish import format_stylish
+from gendiff.formatters.plain import format_plain
+from gendiff.formatters.json import format_json
+from gendiff.parser import parse
 
 
-def generate_diff(file1, file2, format):
-    data = building_diff(file1, file2)
+def to_format(data, format):
     if format == "stylish":
         return format_stylish(data)
     elif format == "plain":
@@ -14,3 +14,8 @@ def generate_diff(file1, file2, format):
         return format_json(data)
     else:
         raise Exception("Incorrect second argument")
+
+
+def generate_diff(file1, file2, format):
+    data = building_diff(parse(file1), parse(file2))
+    return to_format(data, format)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,37 +1,12 @@
-import argparse
 from gendiff.generate_diff import generate_diff
-from gendiff.parser import parse
+from gendiff.cli import cli
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Compares two configuration files and shows a difference."
-        )
-    parser.add_argument("first_file")
-    parser.add_argument("second_file")
-    parser.add_argument(
-        '-f',
-        '--format',
-        type=str,
-        default='stylish',
-        help='set format of output'
-    )
-
-    args = parser.parse_args()
-    data1 = parse(args.first_file)
-    data2 = parse(args.second_file)
-    format_data = args.format
-
-    str_diff = generate_diff(data1, data2, format_data)
+    args = cli()
+    str_diff = generate_diff(args.first_file, args.second_file, args.format)
     print(str_diff)
 
 
 if __name__ == '__main__':
     main()
-
-'''
-generate_diff принимает пути
-main с 7 по 14 отдельная функция, эту функцию поместить в парссер(cli.py)
-пакет(папка) formatters - to json to plain to json убрать приставку to
-generate_diff условия сделать отдельной функцией
-'''
