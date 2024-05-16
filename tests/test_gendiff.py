b687b9f7df43e50810fcbec63d11e38ef5dbbdb8
Fedor.Shurgin <shurginfedor@gmail.com>
2024-05-16 23:51:59 +0300

    fix test

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,11 +2,11 @@ from gendiff.generate_diff import generate_diff
 
 
 def test_gendiff():
-    file1_json = '/home/fedor/Hexlet/python-project-50/tests/fixtures/file3.json'
-    file2_json = '/home/fedor/Hexlet/python-project-50/tests/fixtures/file4.json'
-    result_stylish = open('/home/fedor/Hexlet/python-project-50/tests/fixtures/result_stylish.txt')
-    result_plain = open('/home/fedor/Hexlet/python-project-50/tests/fixtures/result_plain.txt')
-    result_json = open('/home/fedor/Hexlet/python-project-50/tests/fixtures/result_json.txt')
+    file1_json = 'tests/fixtures/file3.json'
+    file2_json = 'tests/fixtures/file4.json'
+    result_stylish = open('tests/fixtures/result_stylish.txt')
+    result_plain = open('tests/fixtures/result_plain.txt')
+    result_json = open('tests/fixtures/result_json.txt')
     assert generate_diff(file1_json, file2_json, format='stylish') == result_stylish.read()
     assert generate_diff(file1_json, file2_json, format='plain') == result_plain.read()
     assert generate_diff(file1_json, file2_json, format='json') == result_json.read()
