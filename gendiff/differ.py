b2a6c4dfc08971b7773f6eecce86291b08ea8091
Fedor.Shurgin <shurginfedor@gmail.com>
2024-05-20 19:41:21 +0300

    add fix

--- gendiff/differ.py
+++ gendiff/differ.py
@@ -3,6 +3,6 @@ from gendiff.parser import parse
 from gendiff.formatters import format_data
 
 
-def generate_diff(file1, file2, format):
+def generate_diff(file1, file2, format='stylish'):
     data = building_diff(parse(file1), parse(file2))
     return format_data(data, format)
